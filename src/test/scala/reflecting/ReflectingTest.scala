aa23090aad9b5e96dac2fbeab610dac766874408
Arjan Blokzijl <arjanblokzijl@gmail.com>
2012-08-28 12:55:10 +0200

    extra append test

--- src/test/scala/reflecting/ReflectingTest.scala
+++ src/test/scala/reflecting/ReflectingTest.scala
@@ -4,7 +4,9 @@ package reflecting
 object ReflectingTest extends App {
   import Reflecting._
 
- println(eval(List(5,6,7,8), "head")) //5
+  println(eval(List(5,6,7,8), "head")) //5
 
- println(eval(List(5,6,7,8), "drop", 2)) //List(7,8)
+  println(eval(List(5,6,7,8), "drop", 2)) //List(7,8)
+
+  println(eval(List(5,6,7,8), "$colon$colon", 2)) //List(2,5,6,7,8)
 }
